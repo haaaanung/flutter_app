3b2bfd778daa66a922d9b6cd2407e2e1cb4f4730
haaaanung <hanung510@naver.com>
2024-09-15 22:06:49 +0900

    good

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,9 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:typed_data';
+import 'dart:io';
 
 void main() {
   runApp(MyApp());
@@ -11,97 +13,274 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Upload Example',
+      title: 'Celebrity Impersonation Ad Classifier',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: UploadImageScreen(),
+      home: InitialScreen(),
     );
   }
 }
 
-class UploadImageScreen extends StatefulWidget {
+class InitialScreen extends StatelessWidget {
   @override
-  _UploadImageScreenState createState() => _UploadImageScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '연예인 사칭 광고 분류 AI 프로그램',
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+              child: Text('Start'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _UploadImageScreenState extends State<UploadImageScreen> {
-  File? _image;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  Future<void> _pickImage() async {
-    final picker = ImagePicker();
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+  List<Map<String, dynamic>> _uploadedImages = [];
 
+  Future<void> _getImage(ImageSource source) async {
+    final pickedFile = await _picker.pickImage(source: source);
     if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
+      final bytes = await pickedFile.readAsBytes();
+      final base64Image = base64Encode(bytes);
+      final result = await _uploadImage(File(pickedFile.path));
+      _showConfirmationDialog(base64Image, result);
     }
   }
 
-  Future<void> _uploadImage() async {
-    if (_image == null) return;
-
-    final uri = Uri.parse('YOUR_UPLOAD_URL_HERE'); // 여기에 업로드할 URL을 입력하세요.
-    final request = http.MultipartRequest('POST', uri)
-      ..files.add(await http.MultipartFile.fromPath(
-        'image',
-        _image!.path,
-      ));
+  Future<String> _uploadImage(File imageFile) async {
+    final request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://192.168.219.113:5000/predict'),
+    );
+    request.files.add(await http.MultipartFile.fromPath('file', imageFile.path));
 
     final response = await request.send();
-
     if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Image uploaded successfully'),
-      ));
+      final responseData = await response.stream.bytesToString();
+      final decodedData = json.decode(responseData);
+      return decodedData['predicted_class'];
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Image upload failed'),
-      ));
+      return 'Error';
     }
+  }
+
+  void _showConfirmationDialog(String base64Image, String initialResult) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String selectedResult = initialResult;
+        return AlertDialog(
+          title: Text('이미지 분류 결과'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.memory(base64Decode(base64Image), height: 200, width: 200),
+              SizedBox(height: 20),
+              Text('AI 분류 결과 $initialResult 입니다.'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Ai가 잘못 분류했나요?'),
+                  DropdownButton<String>(
+                    value: selectedResult,
+                    items: ['실제 광고', '사칭 광고'].map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      if (newValue != null) {
+                        setState(() {
+                          selectedResult = newValue;
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('확인'),
+              onPressed: () {
+                setState(() {
+                  _uploadedImages.add({
+                    'image': base64Image,
+                    'result': selectedResult,
+                  });
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Upload Image Example'),
+        title: Text('Home'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null
-                ? Text(
-                    '이미지를 골라주세요',
-                    style: TextStyle(fontSize: 18),
-                  )
-                : Image.file(_image!),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: _pickImage,
-              icon: Icon(Icons.photo_library),
-              label: Text('사진 가져오기'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                textStyle: TextStyle(fontSize: 16),
-              ),
+      body: Stack(
+        children: <Widget>[
+          PageView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              _buildRepresentativeAdImagesPage(),
+              _buildClassifiedAdsPage(),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => _getImage(ImageSource.camera),
+                        child: Text('카메라'),
+                      ),
+                      SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: () => _getImage(ImageSource.gallery),
+                        child: Text('갤러리'),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '업로드한 내역은 아래로 스와이프',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: _uploadImage,
-              icon: Icon(Icons.cloud_upload),
-              label: Text('사칭 여부 검사'),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                textStyle: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRepresentativeAdImagesPage() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20),
+        Text('대표적인 사칭 광고 이미지', style: TextStyle(fontSize: 18)),
+        SizedBox(
+          height: 500,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/img.png', height: 200, width: 200),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/img_1.png', height: 200, width: 200),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/img_2.png', height: 200, width: 200),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/img_3.png', height: 200, width: 200),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/img_4.png', height: 200, width: 200),
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget _buildClassifiedAdsPage() {
+    return Column(
+      children: <Widget>[
+        Text('실제 광고', style: TextStyle(fontSize: 18)),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: _uploadedImages.length,
+            itemBuilder: (context, index) {
+              final image = _uploadedImages[index];
+              if (image['result'] == '실제 광고') {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.memory(base64Decode(image['image']), height: 200, width: 200),
+                );
+              }
+              return Container();
+            },
+          ),
+        ),
+        Divider(),
+        Text('사칭 광고', style: TextStyle(fontSize: 18)),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: _uploadedImages.length,
+            itemBuilder: (context, index) {
+              final image = _uploadedImages[index];
+              if (image['result'] == '사칭 광고') {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.memory(base64Decode(image['image']), height: 200, width: 200),
+                );
+              }
+              return Container();
+            },
+          ),
+        ),
+        Spacer(),
+      ],
     );
   }
 }
